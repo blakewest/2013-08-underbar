97283939d14003f0423cf665846778a6fda57f57
Blake West <bwest87@gmail.com>
2013-08-03 01:20:43 -0500

    refactored old functions, and added most of the extra credit functions

--- src/underbar.js
+++ src/underbar.js
@@ -15,32 +15,29 @@ var _ = { };
 
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
-  _.first = function(array, n) {             //look up other methods so you don't have to use a new array.
-    var newArray = [];
-    if (n >= 0) {
-      for(var i = 0; i < n; i++) {
-        newArray.push(array[i]);
-      }
-    }else{
+  _.first = function(array, n) {             
+    if (n >= 0 && n < array.length) {           //checking if it's between 0 and length of array.
+      return array.slice(0, n);
+    }else if(n > array.length) {                //if greater than length of array, just return whole array
+      return array;
+    }
+    else {                                      //if n isn't given, return first element of array
       return array[0];
     }
-    return newArray;
+
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    var newArray = [];
-    var lastItem = array.length-1
-    if (n >= 0) {
-      for (var i = lastItem; i >= (array.length - n) && i >= 0; i--) {
-        newArray.push(array[i]);
-      }
-    }else{
-      console.log(newArray);
-      return array[lastItem];
+    if (n >= 0 && n < array.length) {
+      return array.slice(array.length-n);
+    }else if (n > array.length) {
+      return array;
     }
-    return newArray.reverse();
+    else {
+      return array[array.length-1];
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -63,6 +60,13 @@ var _ = { };
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
+    var res;
+    _.each(array, function(item, index) {
+      if (item === target && res === undefined) {
+        return res = index;
+      }
+    })
+      return res || -1 ;
   };
 
   // Return all elements of an array that pass a truth test.
@@ -341,7 +345,17 @@ var _ = { };
   // The arguments for the original function are passed after the wait
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
-  _.delay = function(func, wait) {
+  _.delay = function(func, wait) {                    //talk to someone about this, this seems like a bad answer;
+  var params = [];
+  if (arguments.length > 2) {
+    for (var i = 2; i < arguments.length; i++) {
+      params.push(arguments[i]);
+    }
+    setTimeout(func.apply(this, params), wait);
+  } else {
+    setTimeout(func, wait);
+    }
+
   };
 
 
@@ -352,6 +366,16 @@ var _ = { };
 
   // Shuffle an array.
   _.shuffle = function(array) {
+    var bag = [];
+    var shuffled = [];
+    for (var i = 0; i < array.length; i++) {
+      bag.push(i);
+    }
+    for (var j = 0; j < array.length; j++) {
+      var randomIndex = Math.floor(Math.random() * array.length)
+      shuffled[randomIndex] = array[j];
+    }
+    return shuffled;
   };
 
 
@@ -366,6 +390,15 @@ var _ = { };
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    var cleanCollection = [];
+    var item = _.each(collection, function(item) {
+      var result = iterator(item);
+      if (result) {
+        cleanCollection.push(item);
+      }
+    });
+    console.log(cleanCollection);
+
   };
 
   // Zip together two or more arrays with elements of the same index
@@ -374,23 +407,96 @@ var _ = { };
   // Example:
   // _.zip(['a','b','c','d'], [1,2,3]) returns [['a',1], ['b',2], ['c',3], ['d',undefined]]
   _.zip = function() {
+    //create a new array that contains many arrays, each one containing one of each of that index. 
+    //create master array. 
+    //go through each argument, take the item and if the master array doesn't have that index number, create an array at that index number, and add the item to that.
+    var zippedArray = [];
+    var maxLength = 0;
+    for (var each in arguments) {                            //grabbing max length; feels hacky. don't like this.
+      var length = arguments[each].length;
+      if (length > maxLength) {
+        maxLength = length;
+      }
+    }
+
+    for (var i = 0; i < arguments.length; i++) {            //go through every passed in array
+      var currentArray = arguments[i];                      //just declaring for readability
+      for (var j = 0; j < maxLength; j++) {       
+        if (zippedArray[j] === undefined) {                 //if the array index doesn't exist, create an array at that index and populate with current item
+          zippedArray[j] = [currentArray[j]];
+        }
+        else {
+          zippedArray[j].push(currentArray[j]);             //if the index already exists, then add the current item to the index.
+        }
+      }
+    }
+    return zippedArray;
   };
 
   // Takes a multidimensional array and converts it to a one-dimensional array.
   // The new array should contain all elements of the multidimensional array.
   //
   // Hint: Use Array.isArray to check if something is an array
-  _.flatten = function(nestedArray, result) {        //do this one.
+  _.flatten = function(nestedArray, result) { 
+    var flattened = [];
+    //loop through whole array. if you see an array, recurse down. keep recursing till you find an item, then push that to the flattened.
+    _.each(nestedArray, function(item) {
+      if (Array.isArray(item)) {
+        var temp = _.flatten(item)
+        if (temp) {
+          for (var i = 0; i < temp.length; i++) {
+            flattened.push(temp[i]);
+          }
+        }
+        return flattened;
+      }else {
+        return flattened.push(item);
+      }
+    })
+    return flattened;
   };
 
   // Takes an arbitrary number of arrays and produces an array that contains
   // every item shared between all the passed-in arrays.
   _.intersection = function() {
+    //use arguments to go through each passed in array. 
+    //really, just take first argument, use it as "master". then go through, and check against other arguments.
+    //use splice (index, 1) to remove the item from the list. This changes the original array, which is what we want in this case. 
+    var firstArray = arguments[0]
+    var master = [];
+    for (var k = 0; k < firstArray.length; k++) {
+      master.push(firstArray[k]);
+    }
+
+    for (var i = 1; i < arguments.length; i++) {          //loop through each argument starting with the 2nd
+      var currentArray = arguments[i];                    //just naming for readability
+      for (var j = 0; j < firstArray.length; j++) {           //loop through each item in the current argument.
+        if (currentArray.indexOf(firstArray[j]) === -1) {       
+            master.splice(master.indexOf(firstArray[j]), 1);   //if the current item is not contained in the master array, delete item from the master. 
+        }       
+      }
+    }
+    return master;
   };
 
   // Take the difference between one array and a number of other arrays.
   // Only the elements present in just the first array will remain.
   _.difference = function(array) {
+    var firstArray = arguments[0]
+    var master = [];
+    for (var k = 0; k < firstArray.length; k++) {
+      master.push(firstArray[k]);
+    }
+   
+    for (var i = 1; i < arguments.length; i++) {          //loop through each argument starting with the 2nd
+      var currentArray = arguments[i];                    //just naming for readability
+      for (var j = 0; j < firstArray.length; j++) {           //loop through each item in the current argument.
+        if (currentArray.indexOf(firstArray[j]) != -1) {       
+            master.splice(master.indexOf(firstArray[j]), 1);                          //if the current item is in another array, it's not different, so we'll remove it from the master array. 
+        }
+      }
+    }
+    return master;
   };
 
 
